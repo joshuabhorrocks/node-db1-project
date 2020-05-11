ba6c11c9a987265b6a31c51fc3af352985acb05d
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-05-11 13:55:03 -0600

    MVP reached, working on Stretch

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const db = require("../accounts/account-router");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", db);
+
+server.get("/", (req, res) => {
+    res.status(200).json("Server is running!")
+})
 
 module.exports = server;
